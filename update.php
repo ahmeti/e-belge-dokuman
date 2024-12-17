37877098d3dfcc01f590f12de4d94ff3aba9d576
Ahmet Imamoglu <ahmet363@gmail.com>
2024-12-18 05:40:45 +0700

    Withholding ilave edildi.

--- update.php
+++ update.php
@@ -1,22 +1,35 @@
 <?php
 
+namespace Ahmeti\EBelgeDokuman;
+
+require __DIR__.'/vendor/autoload.php';
+
+use Exception;
+use HeadlessChromium\BrowserFactory;
+use HeadlessChromium\Page;
+use ZipArchive;
+
 class Update
 {
+    private Page $page;
+
     public function downloadFile(string $zipUrl, string $fileName, bool $unzip = false): void
     {
-        $file = fopen (dirname(__FILE__) . '/'.$fileName, 'w+');
+        $file = fopen(dirname(__FILE__).'/'.$fileName, 'w+');
 
         $ch = curl_init($zipUrl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_FILE, $file);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_exec($ch);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        if (curl_exec($ch) === false) {
+            throw new Exception('Curl error: '.curl_error($ch));
+        }
         curl_close($ch);
         fclose($file);
 
-
-        if($unzip){
+        if ($unzip) {
             $zip = new ZipArchive;
             $res = $zip->open($fileName);
             if ($res === true) {
@@ -29,13 +42,68 @@ class Update
         }
     }
 
+    private function crawler(): void
+    {
+        $html = file_get_contents(dirname(__FILE__).'/dummy.html');
+
+        $jsContent = file_get_contents(dirname(__FILE__).'/E_Fatura_Canli_Ext_Base.js');
+        $jsContent .= file_get_contents(dirname(__FILE__).'/E_Fatura_Canli_Ext_All.js');
+        $jsContent .= file_get_contents(dirname(__FILE__).'/E_Fatura_Canli_Core_Main.js');
+        $html = str_replace('<script>//SCRIPTS_CONTENT//</script>', '<script>'.$jsContent.'</script>', $html);
+
+        file_put_contents(dirname(__FILE__).'/dummy-debug.html', $html);
+
+        $browserFactory = new BrowserFactory;
+        $browser = $browserFactory->createBrowser();
+
+        try {
+            $this->page = $browser->createPage();
+            $this->page->setHtml($html);
+
+            $this->createWithholding();
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        } finally {
+            $browser->close();
+        }
+    }
+
+    private function createWithholding(): void
+    {
+        $withholding = [];
+        $names = $this->page->evaluate('kdvTevkifatKodAciklamaListesi')->getReturnValue();
+        $rates = $this->page->evaluate('kdvTevkifatKodOranListesi')->getReturnValue();
+
+        foreach ($names as $name) {
+            $code = substr($name[0], 0, 3);
+            $rateKey = $name[0];
+            $name = $name[1];
+
+            if ($rateKey === '6274') {
+                continue; // Aktif değil! (1/11/2022 tarihi öncesi) Demir-Çelik Ürünlerinin Teslimi [KDVGUT-(I/C-2.1.3.3.8)]
+            }
+
+            if (array_key_exists($code, $withholding)) {
+                throw new Exception('Witholding duplicate code found: '.$code);
+            } else {
+                $withholding[$code] = [
+                    'code' => $code,
+                    'name' => str_replace($code.' - ', '', $name),
+                    'rate' => $rates[$rateKey],
+                ];
+            }
+        }
+        $json = json_encode(array_values($withholding), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        file_put_contents(dirname(__FILE__).'/build/withholding.json', $json);
+    }
+
     private function updateReadme(): void
     {
         date_default_timezone_set('Europe/Istanbul');
 
-        $readme = dirname(__FILE__) . '/README.md';
+        $readme = dirname(__FILE__).'/README.md';
         $file = file($readme);
-        if(!is_array($file)){
+        if (! is_array($file)) {
             throw new Exception('Readme file not found!');
         }
 
@@ -47,7 +115,12 @@ class Update
     {
         echo PHP_EOL.'Update started...'.PHP_EOL;
 
-        $this->downloadFile('https://ebelge.gib.gov.tr/dosyalar/kilavuzlar/e-FaturaPaketi.zip', 'e-FaturaPaketi.zip');
+        $this->downloadFile('https://ebelge.gib.gov.tr/dosyalar/kilavuzlar/e-FaturaPaketi.zip', 'e-FaturaPaketi.zip', true);
+
+        $this->downloadFile('https://portal.efatura.gov.tr/efatura/extjs/adapter/ext/ext-base.js', 'E_Fatura_Canli_Ext_Base.js');
+        $this->downloadFile('https://portal.efatura.gov.tr/efatura/extjs/ext-all.js', 'E_Fatura_Canli_Ext_All.js');
+        $this->downloadFile('https://portal.efatura.gov.tr/efatura/js/core-main.js', 'E_Fatura_Canli_Core_Main.js');
+
         $this->downloadFile('https://api.fitbulut.com/servis/assets/docs/Sovos%20Bulut%20e-Fatura%20WS%20API%20v2.2.zip', 'SovosFatura.zip', true);
         $this->downloadFile('https://api.fitbulut.com/servis/assets/docs/Sovos%20Bulut%20e-Ar%C5%9Fiv%20Fatura%20WS%20API%202.2.zip', 'SovosArsiv.zip', true);
         $this->downloadFile('https://api.fitbulut.com/servis/assets/docs/Sovos%20Bulut%20e-%C4%B0rsaliye%20WS%20API.zip', 'SovosIrsaliye.zip', true);
@@ -55,8 +128,10 @@ class Update
         $this->downloadFile('https://api.fitbulut.com/servis/assets/docs/Sovos%20R&D%20-%20UBL-TR%20Catalogue.xlsx', 'Sovos_UBL_TR_Catalogue.xlsx');
         $this->downloadFile('https://api.fitbulut.com/servis/assets/docs/Sovos%20R&D%20-S%C4%B1k%20Sorulan%20Sorular.xlsx', 'Sovos_Sorulan_Sorular.xlsx');
 
+        $this->crawler();
+
         $this->updateReadme();
-        
+
         echo 'Update finished...'.PHP_EOL;
     }
 }
